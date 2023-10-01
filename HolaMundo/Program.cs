0685ba4de6c8237efcaca6abab41acdd5336bb7f
JoseEduardoHernandezAlvarado <jeduardoha08@gmail.com>
2023-10-01 15:56:53 -0700

    first

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,77 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double x;double y;
+        int Cantidad;
+        char opc;
+        Operaciones llamar =new Operaciones();
+        
+
+        //Hola mundo
+       Console.WriteLine("Ingrese cantidad de veces para repetir el mensaje");
+        Cantidad = int.Parse(Console.ReadLine());
+        for(int c=0;c<Cantidad;c++){
+            Console.WriteLine((c+1)+": Hola,Bienvenido a mi primer proyecto en visual code");
+        }
+        Console.ReadKey();
+        //inicio del proyecto calculadora de 2 numeros 
+       do{ 
+        Console.Clear();
+        Console.WriteLine("\n\nEliga una opcion"+
+       "\nS :Suma"+
+       "\nR :Resta"+
+       "\nM :Multiplicacion"+
+       "\nD :Division"+
+       "\nF :Finalizar Programa");
+       opc=Console.ReadKey().KeyChar;
+       
+        switch (opc){
+            case 'S':
+            Console.WriteLine("\nIngrese X");
+            x=double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Y");
+            y=double.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado es :");
+            llamar.Suma(x,y);
+            Console.ReadKey();
+            break;
+            case 'R':
+             Console.WriteLine("\nIngrese X");
+            x=double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Y");
+            y=double.Parse(Console.ReadLine());
+              Console.WriteLine("El resultado es :");
+            llamar.Resta(x,y);
+            Console.ReadKey();
+            break;
+            case 'M':
+              Console.WriteLine("\nIngrese X");
+            x=double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Y");
+            y=double.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado es :");
+            llamar.Multiplicacion(x,y);
+            Console.ReadKey();
+            break;
+            case 'D':
+              Console.WriteLine("\nIngrese X");
+            x=double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Y");
+            y=double.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado es :");
+            llamar.Division(x,y);
+            Console.ReadKey();
+            break;
+            case 'F':
+            Console.WriteLine("Gracias por usar el programa presione cualquier tecla para continuar");
+            Console.ReadKey();
+            break;
+            default:
+            Console.WriteLine("Opcion no valida favor de volver a ingresar opcion");
+            break;
+        }
+       }while(opc != 'F');    
+    }
+}
---
+++ HolaMundo/class Operaciones(){.cs
@@ -0,0 +1,14 @@
+class Operaciones{
+public void Suma(double x, double y){
+  Console.WriteLine(x+y);
+}
+public void Resta(double  x,double y){
+    Console.WriteLine(x-y);
+}
+public void Multiplicacion(double x,double y){
+    Console.WriteLine(x*y);
+}
+public void Division(double x,double y){
+    Console.WriteLine(x/y);
+}
+}
